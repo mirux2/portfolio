7b6dbffd354363ea10e92e4b987a0c0ddd5f936d
adachi hikaru <hikaru@adachinoMacBook-Air.local>
2025-11-19 02:51:22 +0900

    delete spa.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-document.addEventListener("DOMContentLoaded",function(){
-
+document.addEventListener("DOMContentLoaded", function () {
   // ハンバーガーメニュー制御
   const menuToggle = document.querySelector("#menuToggle");
   const navLinks = document.querySelector("#navLinks");
@@ -8,31 +7,29 @@ document.addEventListener("DOMContentLoaded",function(){
   });
 
   // ヒーロースライダー制御
-  const swiper = new Swiper('.swiper', {
+  const swiper = new Swiper(".swiper", {
     loop: true,
     pagination: {
-      el: '.swiper-pagination',
-    }
+      el: ".swiper-pagination",
+    },
   });
 
   //タイトルアニメーション制御
   //スクロールオブザーバー制御
-  const ta = (el,isIntersecting) => {
-    if(isIntersecting){
+  const ta = (el, isIntersecting) => {
+    if (isIntersecting) {
       const title = new TextAnimation(el);
       title.animate();
     }
-  }
-  const fa = (el,isIntersecting) => {
-    if(isIntersecting){
+  };
+  const fa = (el, isIntersecting) => {
+    if (isIntersecting) {
       const float = new floatAnimation(el);
       float.animate();
     }
-  }
-  const titleobserver = new ScrollObserver(".title",ta);
-  const floatobserver = new ScrollObserver(".info",fa);
+  };
+  const titleobserver = new ScrollObserver(".title", ta);
+  const floatobserver = new ScrollObserver(".info", fa);
 
   //SPA
-  const spa = new SinglePage(".work-cards","#modal","close-btn");
-
 });
