8ef415ef9e8931cacd0786ab05cf406df0f8d85d
adachi hikaru <hikaru@adachinoMacBook-Air.local>
2026-01-04 13:09:22 +0900

    skills layout changed, mobile menu active remove option and background curtain added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
   // ハンバーガーメニュー制御
+  const header = document.querySelector(".jsHeader");
   const menuToggle = document.querySelector("#menuToggle");
   const navLinks = document.querySelector("#navLinks");
+  const cover = document.querySelector(".curtain");
   menuToggle.addEventListener("click", () => {
+    header.classList.toggle("active");
     navLinks.classList.toggle("active");
     menuToggle.classList.toggle("active");
+    cover.classList.toggle("active");
+  });
+  navLinks.querySelectorAll("a").forEach((a) => {
+    a.addEventListener("click", () => {
+      menuToggle.classList.remove("active");
+      navLinks.classList.remove("active");
+      header.classList.remove("active");
+      cover.classList.remove("active");
+    });
+  });
+  document.addEventListener("click", (e) => {
+    if (!navLinks.contains(e.target) && !menuToggle.contains(e.target)) {
+      menuToggle.classList.remove("active");
+      navLinks.classList.remove("active");
+      header.classList.remove("active");
+      cover.classList.remove("active");
+    }
   });
 
   // ヒーロースライダー制御
